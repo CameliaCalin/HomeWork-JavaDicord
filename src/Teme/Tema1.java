6b29efb699e0febf084a99c5cad21bd61fe23d65
Cami <calincamelia03@gmail.com>
2024-04-07 00:11:13 +0300

    update

---
+++ src/Teme/Tema1.java
@@ -0,0 +1,14 @@
+package Teme;
+
+public class Tema1 { //Tema 1
+    public static void main(String[] args) {
+        int x = 3;
+        int y = 4;
+        int z = 100;
+        String nume = "Nume";
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(z);
+        System.out.println(nume);
+    }
+}
